ecf547e02293937eebce6c97ab8e22b0b30946ca
Mohammad Haroon <103740969+Haroon500@users.noreply.github.com>
2022-10-21 10:27:02 -0700

    Update Coll.java

--- src/homework/Coll.java
+++ src/homework/Coll.java
@@ -3,7 +3,41 @@ package homework;
 public class Coll {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			ArrayList<String> list=new ArrayList<String>();
+		LinkedList<String> al=new LinkedList<String>(); 
+		Stack<String> stack = new Stack<String>();  
+		list.add("haroon"); 
+		list.add("khan");  
+		list.add("haroon");  
+		list.add("kaka");  	 
+		Iterator itr=list.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}
+		System.out.println("--------------------------------------------------------------------");
+		al.add("ahmad");  
+		al.add("rashid");  
+		al.add("abobakar");  
+		al.add("ali");  
+		Iterator<String> itr1=al.iterator();  
+		while(itr1.hasNext()){  
+		System.out.println(itr1.next());  
+		}
+		
+System.out.println("------------------------------------------------------------------------------");
+
+
+
+stack.push("wali");  
+stack.push("addff");  
+stack.push("fdgds");  
+stack.push("rwewr");  
+stack.push("erewre");  
+stack.pop();  
+Iterator<String> itr2=stack.iterator();  
+while(itr2.hasNext()){  
+System.out.println(itr2.next());  
+}
 
 	}
 
