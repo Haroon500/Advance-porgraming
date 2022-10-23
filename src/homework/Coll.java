c566d7b7762f12fc2a82acba54dc18a98c5de33a
Mohammad Haroon <103740969+Haroon500@users.noreply.github.com>
2022-10-23 03:57:37 -0700

    Update Coll.java

--- src/homework/Coll.java
+++ src/homework/Coll.java
@@ -6,6 +6,7 @@ public class Coll {
 			ArrayList<String> list=new ArrayList<String>();
 		LinkedList<String> al=new LinkedList<String>(); 
 		Stack<String> stack = new Stack<String>();  
+		Vector<String> v=new Vector<String>(); 
 		list.add("haroon"); 
 		list.add("khan");  
 		list.add("haroon");  
@@ -36,9 +37,42 @@ stack.push("erewre");
 stack.pop();  
 Iterator<String> itr2=stack.iterator();  
 while(itr2.hasNext()){  
-System.out.println(itr2.next());  
+System.out.println(itr2.next()); 
 }
+		
+System.out.println("------------------------------------------------------------------------------");
+ 
+v.add("mama");  
+v.add("e");  
+v.add("f");  
+v.add("G");  
+Iterator<String> it3r=v.iterator();  
+while(itr3.hasNext()){  
+System.out.println(itr3.next());  
+} 
+		PriorityQueue<String> queue=new PriorityQueue<String>();  
+queue.add("Amit Sharma");  
+queue.add("Vijay Raj");  
+queue.add("JaiShankar");  
+queue.add("Raj");  
+System.out.println("head:"+queue.element());  
+System.out.println("head:"+queue.peek());  
+System.out.println("iterating the queue elements:");  
+Iterator itr4=queue.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+queue.remove();  
+queue.poll();  
+System.out.println("after removing two elements:");  
+Iterator<String> itr6=queue.iterator();  
+while(itr4.hasNext()){  
+System.out.println(itr4.next());  
+}  
+	
 
+	}
+		
 	}
 
 }
